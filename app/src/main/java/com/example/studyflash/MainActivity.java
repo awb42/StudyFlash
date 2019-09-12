9a4c6587a9769e5377493a4ec02b873ff5efbf3d
awb42 <awb42@live.missouristate.edu>
2019-09-12 13:06:26 -0500

    Created the StudyFlash repo

---
+++ app/src/main/java/com/example/studyflash/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.studyflash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AnimatorSet mSetRightOut;
+    private AnimatorSet mSetLeftIn;
+    private boolean mIsBackVisible = false;
+    private View mCardFrontLayout;
+    private View mCardBackLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViews();
+        loadAnimations();
+        changeCameraDistance();
+    }
+
+    private void changeCameraDistance(){
+        int distance = 8000;
+        float scale = getResources().getDisplayMetrics().density * distance;
+        mCardFrontLayout.setCameraDistance(scale);
+        mCardBackLayout.setCameraDistance(scale);
+    }
+
+    private void loadAnimations(){
+        mSetRightOut = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.flibbackwardanimation);
+        mSetLeftIn = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.flipforwardanimation);
+    }
+
+    private void findViews(){
+        mCardBackLayout = findViewById(R.id.cardBack);
+        mCardFrontLayout = findViewById(R.id.cardFront);
+    }
+
+    public void flipCard(View view) {
+        if (!mIsBackVisible){
+            mSetRightOut.setTarget(mCardFrontLayout);
+            mSetLeftIn.setTarget(mCardBackLayout);
+            mSetRightOut.start();
+            mSetLeftIn.start();
+            mIsBackVisible = true;
+        }
+        else{
+            mSetRightOut.setTarget(mCardBackLayout);
+            mSetLeftIn.setTarget(mCardFrontLayout);
+            mSetRightOut.start();
+            mSetLeftIn.start();
+            mIsBackVisible = false;
+        }
+    }
+}
