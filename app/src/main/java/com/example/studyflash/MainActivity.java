f62df64564788c3f3632042f01765306809c6129
awb42 <awb42@live.missouristate.edu>
2019-09-17 20:13:26 -0500

    Added splash screen and Question 1 text.

--- app/src/main/java/com/example/studyflash/MainActivity.java
+++ app/src/main/java/com/example/studyflash/MainActivity.java
@@ -1,60 +1,33 @@
 package com.example.studyflash;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.animation.AnimatorInflater;
-import android.animation.AnimatorSet;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button flashCardButton;
 
-    private AnimatorSet mSetRightOut;
-    private AnimatorSet mSetLeftIn;
-    private boolean mIsBackVisible = false;
-    private View mCardFrontLayout;
-    private View mCardBackLayout;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViews();
-        loadAnimations();
-        changeCameraDistance();
+
+        flashCardButton = findViewById(R.id.flashCardButton);
+        flashCardButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                flashCardActivity();
+            }
+        });
     }
 
-    private void changeCameraDistance(){
-        int distance = 8000;
-        float scale = getResources().getDisplayMetrics().density * distance;
-        mCardFrontLayout.setCameraDistance(scale);
-        mCardBackLayout.setCameraDistance(scale);
-    }
-
-    private void loadAnimations(){
-        mSetRightOut = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.flibbackwardanimation);
-        mSetLeftIn = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.flipforwardanimation);
-    }
-
-    private void findViews(){
-        mCardBackLayout = findViewById(R.id.cardBack);
-        mCardFrontLayout = findViewById(R.id.cardFront);
-    }
-
-    public void flipCard(View view) {
-        if (!mIsBackVisible){
-            mSetRightOut.setTarget(mCardFrontLayout);
-            mSetLeftIn.setTarget(mCardBackLayout);
-            mSetRightOut.start();
-            mSetLeftIn.start();
-            mIsBackVisible = true;
-        }
-        else{
-            mSetRightOut.setTarget(mCardBackLayout);
-            mSetLeftIn.setTarget(mCardFrontLayout);
-            mSetRightOut.start();
-            mSetLeftIn.start();
-            mIsBackVisible = false;
-        }
+    public void flashCardActivity()
+    {
+        Intent intent = new Intent(this, flashCardActivity.class);
+        startActivity(intent);
     }
 }
